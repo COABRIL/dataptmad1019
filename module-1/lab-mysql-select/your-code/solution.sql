fc47bf63ada2f0fc22801ad64834809b50b4239a
coabril <cortega@alsa.es>
2019-11-17 20:41:49 +0100

    finished

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -1,41 +1,69 @@
-/*Challenge1:
- * Easy way:*/
-select authors.au_id, 
-au_lname, 
-au_fname, 
-titles.title, publishers.pub_name
-from titles
-JOIN titleauthor
-on titles.title_id = titleauthor.title_id
-JOIN authors
-on authors.au_id = titleauthor.au_id
-join publishers 
-on publishers.pub_id = titles.pub_id
+/*Challenge1:*/
+select count (au_id)
+FROM titleauthor;
 
-/*Complicated way with "with":*/
-WITH
-challenge_1A AS
-(
-select *
-from titles
+select authors.au_id AS 'AUTHOR ID', 
+au_lname AS 'LAST NAME', 
+au_fname AS 'FIRST NAME', 
+titles.title AS 'TITLE', publishers.pub_name AS'PUBLISHERS'
+from authors
 JOIN titleauthor
-on titles.title_id = titleauthor.title_id
-JOIN authors
-on authors.au_id = titleauthor.au_id
-),
-challenge_1B as
-(
-select *
-from challenge_1A
+on titleauthor.au_id = authors.au_id
+JOIN titles
+on titleauthor.title_id = titles.title_id
 join publishers 
-on publishers.pub_id = challenge_1A.pub_id
-)
-select au_id, 
-au_lname, 
-au_fname, 
-title, pub_name from challenge_1B;
+on publishers.pub_id = titles.pub_id;
+
+
 
 /*Challenge 2:*/
+select authors.au_id AS 'AUTHOR ID', 
+au_lname AS 'LAST NAME', 
+au_fname AS 'FIRST NAME', 
+titles.title AS 'TITLE', publishers.pub_name AS'PUBLISHERS', COUNT (title) as 'TITLE COUNT'
+from authors
+left JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+left JOIN titles
+on titleauthor.title_id = titles.title_id
+left join publishers 
+on publishers.pub_id = titles.pub_id
+GROUP BY authors.au_id;
 
 
+/*Challenge 3:*/
+select sum (ytd_sales)
+from titles;
 
+select authors.au_id AS 'AUTHOR ID', 
+au_lname AS 'LAST NAME', 
+au_fname AS 'FIRST NAME', 
+titles.title AS 'TITLE', publishers.pub_name AS'PUBLISHERS', sum(titles.ytd_sales) as 'SALES'
+from authors
+left JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+left JOIN titles
+on titleauthor.title_id = titles.title_id
+left join publishers 
+on publishers.pub_id = titles.pub_id
+GROUP BY authors.au_id
+ORDER BY sum (titles.ytd_sales) DESC
+limit 3;
+
+
+/*Challenge 4:*/
+select authors.au_id AS 'AUTHOR ID', 
+au_lname AS 'LAST NAME', 
+au_fname AS 'FIRST NAME', 
+titles.title AS 'TITLE', publishers.pub_name AS'PUBLISHERS', sum(titles.ytd_sales) as 'SALES'
+from authors
+left JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+left JOIN titles
+on titleauthor.title_id = titles.title_id
+left join publishers 
+on publishers.pub_id = titles.pub_id
+GROUP BY authors.au_id
+ORDER BY sum (titles.ytd_sales) DESC;
+
+/*-------------------------------------------------------*/
